c382c20b18a56f6d45e6a28de1b0790a055bec2f
alex <al3xback@gmail.com>
2024-02-09 12:01:15 +0700

    added loading state

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import { sendHttpRequest } from './util.js';
 
 const cardsEl = document.querySelector('.cards');
 const cardTemplate = document.getElementById('card-template');
+const loadingEl = document.querySelector('.loading');
 
 const URL =
 	'https://gist.githubusercontent.com/al3xback/1818bcbc392fcf68d6837ac6a18692a7/raw/bb1d782ac93a4ce921536bba248431eddf462378/3-column-data.json';
@@ -9,11 +10,16 @@ const URL =
 const renderCardsContent = (data) => {
 	const carsData = JSON.parse(data);
 
+	loadingEl.parentElement.removeChild(loadingEl);
+
 	for (const car in carsData) {
 		carsData[car].name = car;
 		const { name, image, description } = carsData[car];
 
-		const cardTemplateNode = document.importNode(cardTemplate.content, true);
+		const cardTemplateNode = document.importNode(
+			cardTemplate.content,
+			true
+		);
 		const cardEl = cardTemplateNode.querySelector('.card');
 		cardEl.classList.add('card--' + name);
 
@@ -26,7 +32,7 @@ const renderCardsContent = (data) => {
 		const cardDescEl = cardEl.querySelector('.card__desc');
 		cardDescEl.textContent = description;
 
-		cardsEl.appendChild(cardEl);
+		cardsEl.appendChild(cardTemplateNode);
 	}
 };
 
